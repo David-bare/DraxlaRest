40e2d106d8ee7ee659343b9840e10c0134fbd3cf
Ogunlaja David Eyitimibare <davidogunlaja.com>
2024-10-13 15:35:30 +0100

    Initial commit

---
+++ Models/Authentication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraxlaRest.Models
+{
+    public class Authentication
+    {
+        public string userid { get; set; }
+        public string accesscode { get; set; }
+        public string sno { get; set; }
+    }
+}
---
+++ Models/StaffDetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DraxlaRest.Models
+{
+    public class StaffDetails
+    {
+        public string role { get; set; }
+        public string branch { get; set; }
+        public string staff_name { get; set; }
+        public string fullname { get; set; }
+        public int usertype { get; set; }
+        public int response_code { get; set; }
+        public string response_description { get; set; }
+
+        //globus
+        //public object businessPhones { get; set; }
+        //public string displayName { get; set; }
+        //public string givenName { get; set; }
+        //public string jobTitle { get; set; }
+        //public string mail { get; set; }
+        //public object mobilePhone { get; set; }
+        //public object officeLocation { get; set; }
+        //public object preferredLanguage { get; set; }
+        //public string surname { get; set; }
+        //public string userPrincipalName { get; set; }
+        //public object id { get; set; }
+        //public string employeeId { get; set; }
+        //public object department { get; set; }
+        //public string message { get; set; }
+        //public bool success { get; set; }
+    }
+}
